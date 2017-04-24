5d37a8335f64a0d864ae728d72ebc33ed79d56fa
bm0 <bm0@soft-way.biz>
2017-04-25 01:58:01 +0300

    bug fixing, handling Vault response status codes

--- README.md
+++ README.md
@@ -10,6 +10,7 @@ Example
         vault_addr: http://127.0.0.1:8200/v1/
         path: secret/foo
       register: foo
+      no_log: yes   # Set if you want to hide the module output
 
     - debug: var=foo.top_secret
 ```
--- library/vault.py
+++ library/vault.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python
-# coding=utf-8
 
-import os
-import json
 from urlparse import urljoin
 from ansible.module_utils.basic import *
 from ansible.module_utils import urls
@@ -15,27 +12,39 @@ def main():
     }
     module = AnsibleModule(argument_spec=fields)
 
-    vault_token = os.getenv('VAULT_TOKEN')
-    vault_addr = module.params.get('vault_addr', os.getenv('VAULT_ADDR'))
+    vault_token = os.environ.get('VAULT_TOKEN')
+    vault_addr = module.params['vault_addr'] or os.environ.get('VAULT_ADDR')
 
     if not vault_token:
-        raise RuntimeError('You must provide environment variable VAULT_TOKEN!')
+        module.fail_json(msg='You must provide environment variable VAULT_TOKEN!')
     if not vault_addr:
-        raise RuntimeError('You must provide an address to Vault server'
-                           ' via the VAULT_ADDR environment variable or as a module parameter!')
+        module.fail_json(msg='You must provide an address to Vault server '
+                             'via the VAULT_ADDR environment variable or as a module parameter!')
+
+    status_message_map = {
+        204: 'The request is successful, but nothing has been returned.',
+        400: 'Invalid request, missing or invalid data.',
+        403: 'Forbidden, your authentication details are either incorrect or you don\'t have access to this feature.',
+        404: 'Invalid path. This can both mean that the path truly doesn\'t exist or '
+             'that you don\'t have permission to view a specific path.',
+        429: 'Default return code for health status of standby nodes, indicating a warning.',
+        500: 'Internal server error. An internal error has occurred, try again later.'
+             'If the error persists, report a bug.',
+        503: ' Vault is down for maintenance or is currently sealed. Try again later.',
+    }
 
     headers = {'X-Vault-Token': vault_token}
     url = urljoin(vault_addr, module.params['path'])
     response, info = urls.fetch_url(module, url, headers=headers)
 
-    if info['status'] > 299:
-        raise RuntimeError('Error, status code: %s' % info['status'])
+    if info['status'] in status_message_map:
+        module.fail_json(msg=status_message_map[info['status']])
 
     content = response.read()
     data = json.loads(content)
 
     if 'data' not in data:
-        raise RuntimeError('Key data was not found in response! Response: %s' % response)
+        module.fail_json(msg='Key data was not found in response! Response: %s' % response)
 
     module.exit_json(**data['data'])
 
